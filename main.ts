703457beff38a2fc17168f8f824cd0758ebf0fd0
Emre-Guzel-2 <yunus.emre.guzel@mths.ca>
2024-10-09 11:24:13 -0400

    aded commit the coments and added the shake functoin

--- main.ts
+++ main.ts
@@ -1,8 +1,25 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
- * Created by: Mr. Coxall
- * Created on: Sep 2020
- * This program ...
+ * Created by: Emre Guzel
+ * Created on: Oct 9 2024
+ * This program alows yu play rock paper sisors 
 */
 
-basic.showString('Hello, World!')
+//seting up the verible
+let socre = 0
+let ramdomNumber: number = 0
+
+basic.clearScreen()
+basic.showIcon(IconNames.Happy)
+
+input.onGesture(Gesture.Shake,function(){
+    ramdomNumber=randint(0,1)
+    basic.clearScreen()
+    
+    
+})
+
+
+
+
+
