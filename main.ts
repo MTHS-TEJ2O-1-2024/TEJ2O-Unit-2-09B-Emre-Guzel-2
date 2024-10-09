dbf96b5077c74d7dc702035365ee263f8d60993e
Emre-Guzel-2 <yunus.emre.guzel@mths.ca>
2024-10-09 13:30:45 -0400

    updated the icons

--- main.ts
+++ main.ts
@@ -9,16 +9,26 @@
 let socre = 0
 let ramdomNumber: number = 0
 
+ramdomNumber = randint(0, 2)
 basic.clearScreen()
 basic.showIcon(IconNames.Happy)
 
 input.onGesture(Gesture.Shake,function(){
-    ramdomNumber=randint(0,1)
-    basic.clearScreen()
-    
-    
-})
+    if (ramdomNumber == 0) {
+        basic.clearScreen()
+        basic.showIcon(IconNames.Square)
+        pause(5000)        
+    }
+    if(ramdomNumber ==1){
+        basic.showIcon(IconNames.Happy)
+        pause(5000)
+        basic.showIcon(IconNames.Diamond)
+    }
 
+})
+   input.onButtonPressed(Button.A,function(){      
+ 
+})
 
 
 
