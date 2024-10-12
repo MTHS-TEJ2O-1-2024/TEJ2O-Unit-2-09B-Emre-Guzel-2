c65b207d19b8afe994471899881b12ff6408140e
Emre-Guzel-2 <yunus.emre.guzel@mths.ca>
2024-10-11 21:37:54 -0400

    Finished the rock paper succors game

--- main.ts
+++ main.ts
@@ -4,32 +4,48 @@
  * Created on: Oct 9 2024
  * This program alows yu play rock paper sisors 
 */
-
-//seting up the verible
-let socre = 0
-let ramdomNumber: number = 0
-
-ramdomNumber = randint(0, 2)
+// Setting up the variable
+let score: number = 0
+// Setting the screen 
 basic.clearScreen()
 basic.showIcon(IconNames.Happy)
 
 input.onGesture(Gesture.Shake,function(){
+    // Picking a ramdom number 
+    let ramdomNumber: number = 0
+    ramdomNumber = -1
+    ramdomNumber = randint(2, 0)
+    // Making the icons for random number 
+    // Rock
     if (ramdomNumber == 0) {
         basic.clearScreen()
-        basic.showIcon(IconNames.Square)
-        pause(5000)        
-    }
-    if(ramdomNumber ==1){
-        basic.showIcon(IconNames.Happy)
+        basic.showIcon(IconNames.SmallSquare)
         pause(5000)
-        basic.showIcon(IconNames.Diamond)
+        basic.showIcon(IconNames.Happy)
+    }
+    // Paper
+    if(ramdomNumber ==1){
+        basic.showIcon(IconNames.Square)
+        pause(5000)
+        basic.showIcon(IconNames.Happy)
+    }
+    // Scissors
+    if(ramdomNumber ==2){
+        basic.showIcon(IconNames.Scissors)
+        pause(5000)
+        basic.showIcon(IconNames.Happy)
     }
 
 })
-   input.onButtonPressed(Button.A,function(){      
- 
+// Save the scroe
+input.onButtonPressed(Button.A,function() { 
+    basic.showIcon(IconNames.Yes)
+    score +=1
 })
 
-
-
+// Show score 
+input.onButtonPressed(Button.B,function (){
+basic.showString(" Score:"), + basic.showNumber(score) 
+basic.showIcon(IconNames.Happy)
+})
 
